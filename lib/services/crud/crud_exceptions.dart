5f098d5397ad7ea81ab76d1f27942a1569025395
Mohammad Zgoul <moeazgoul@gmail.com>
2023-07-19 22:12:04 +0300

    step-11

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,19 @@
+// ** Custom exceptions for notes_service.dart **
+
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
